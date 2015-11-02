6c90e55a024c4531e9d691a345b818a41010120b
Melanie Sedda <melaniesedda@gmail.com>
2015-11-02 20:11:44 +0100

    long int

--- code/archives/main2.cpp
+++ code/archives/main2.cpp
@@ -14,9 +14,9 @@
 #include <algorithm>
 using namespace std;
 
-int print = 0;
-int print_time = 0;
-int print_length = 0;
+int print = 1;
+int print_time = 1;
+int print_length = 1;
 
 // Initialize a matrix AxB
 double** init_matrix_double(int A, int B) {
@@ -75,30 +75,70 @@ long int** compute_distances(int N, double **points) {
 }
 
 // Enhance existent tour
-pair<long int, int *> enhance(int N, long int** distances, long int length_tour, int* tour){
+pair<long int, int *> enhance(int N, long int** distances, long int length_tour, pair<int, int> edges[]){
+    // Enhance
     int cont = 1;
-    int r, k, s, enhancement, temp;
+    //int r, k, s, enhancement, temp;
+    int i, j, enhancement, temp;
     while(cont){
         cont = 0;
-        for (r=1; r < (N+1)/2; r++) {
-            for (k = 0; k < N; k++) {
-                enhancement = distances[tour[k]][tour[(k+1)%N]] + distances[tour[(k+1+r)%N]][tour[(k+2+r)%N]] - distances[tour[k]][tour[(k+1+r)%N]] - distances[tour[(k+1)%N]][tour[(k+2+r)%N]];
-                if (enhancement > 0) {
-                    cont = 1;
-                    // printf("Enhance for %d (r = %d)\n", enhancement, r);
-                    // We switch k+1 and k+1+r, k+2 and k+r, etc
-                    for (s = 1; k+s < k+2+r-s; s++) {
-                        temp = tour[(k+s)%N];
-                        tour[(k+s)%N] = tour[(k+2+r-s)%N];
-                        tour[(k+2+r-s)%N] = temp;
+        for (i=0; i < N; i++){
+            if (print)
+                printf("i = %d\n", i);
+            for (j=i+1; j < N; j++){
+                if (edges[i].second != edges[j].second && edges[j].first != edges[i].first){
+                    enhancement = distances[edges[i].first][edges[i].second] + distances[edges[j].first][edges[j].second] - distances[edges[j].second][edges[i].second] - distances[edges[j].first][edges[i].first];
+                    if (enhancement > 0){
+                        if (print)
+                            printf("enhancement = %d\n", enhancement);
+                        //cont = 1;
+                        // We switch
+                        if (print)
+                            printf("switch : (%d, %d) and (%d, %d)", edges[i].first, edges[i].second, edges[j].first, edges[j].second);
+                        temp = edges[i].first;
+                        edges[i].first = edges[j].second;
+                        edges[j].second = temp;
+                        if (print)
+                            printf("-> (%d, %d) and (%d, %d)\n", edges[i].first, edges[i].second, edges[j].first, edges[j].second);
+                        length_tour -= enhancement;
                     }
-                    length_tour -= enhancement;
                 }
             }
         }
     }
+    // Construct neighbors
+    vector <int> neighbors[1000];
+    for (i=0; i < N; i++){
+        neighbors[edges[i].first].push_back(edges[i].second);
+        neighbors[edges[i].second].push_back(edges[i].first);
+        if (print){
+            printf("Add neighbor : %d : %d\n", edges[i].first, edges[i].second);
+            printf("Add neighbor : %d : %d\n", edges[i].second, edges[i].first);
+        }
+    }
+
+    // Construct tour
+    int *tour = (int *)malloc(N*sizeof(int));
+    int current = 0;
+    int prev = 0;
+    int next;
+    tour[0] = 0;
+    if (print)
+        printf("0\n");
+    for (int k = 1; k<N; k++){
+        if (neighbors[current][0] == prev)
+            next = neighbors[current][1];
+        else
+            next = neighbors[current][0];
+        if (print)
+            printf("prev = %d, current = %d, next = %d\n", prev, current, next);
+        prev = current;
+        current = next;
+        tour[k] = next;
+    }
+
     if (print) {
-        for (k = 0; k < N; k++){
+        for (int k = 0; k < N; k++){
             printf("%d\n", tour[k]);
         }
     }
@@ -110,32 +150,32 @@ pair<long int, int *> enhance(int N, long int** distances, long int length_tour,
 // Nearest neighbor tour
 pair<long int, int *> nearest_neighbor(int N, double** points, long int** distances) {
     if (print)
-        printf("greedy_tour\n");
+        printf("nearest_neighbor\n");
     clock_t start = clock();
-    int *tour = (int *)malloc(N*sizeof(int));
     int *used = (int *)calloc(N, sizeof(int));
+    pair<int, int> edges[1000];
     long int length_tour = 0;
-    tour[0] = 0;
-    if (print)
-        printf("0\n");
-    if (print)
-        printf("Point 0 of the tour: 0\n");
+    edges[0].first = 0;
     used[0] = 1;
     int best,i,j;
     for (i=1; i<N; i++) {
        best = -1;
        for (j=1; j<N; j++) {
-          if (used[j]==0 && (best == -1 || distances[tour[i-1]][j] < distances[tour[i-1]][best])){
+          if (used[j]==0 && (best == -1 || distances[edges[i-1].first][j] < distances[edges[i-1].first][best])){
              best = j;
           }
        }
-       tour[i] = best;
-       length_tour += distances[tour[i-1]][tour[i]];
+       edges[i-1].second = best;
+       edges[i].first = best;
+       length_tour += distances[edges[i-1].first][best];
        if (print)
-          printf("%d\n", best);
+          printf("(%d, %d)\n", edges[i-1].first, best);
        used[best] = 1;
     }
-    length_tour += distances[tour[N-1]][tour[0]];
+    edges[N-1].second = 0;
+    length_tour += distances[edges[N-1].first][0];
+    if (print)
+        printf("(%d, %d)\n", edges[N-1].first, 0);
     if (print_length)
         printf("Length tour = %ld\n", length_tour);
     if (print_time) {
@@ -143,7 +183,7 @@ pair<long int, int *> nearest_neighbor(int N, double** points, long int** distan
         long elapsed = timediff(start, end);
         printf("Nearest neighbor took %ld microseconds\n", elapsed);
     }
-    return enhance(N, distances, length_tour, tour);
+    return enhance(N, distances, length_tour, edges);
 }
 
 // Greedy tour
@@ -151,8 +191,8 @@ pair<long int, int *> greedy(int N, double** points, long int** distances) {
     if (print)
         printf("greedy_tour\n");
     clock_t start_greedy = clock();
-    int *tour = (int *)malloc(N*sizeof(int));
     int *degrees = (int *)calloc(N, sizeof(int));
+    pair<int, int> edges[1000];
     long int length_tour = 0;
     int i,j,k,r;
 
@@ -175,7 +215,6 @@ pair<long int, int *> greedy(int N, double** points, long int** distances) {
 
     // Union find
     int *sets = (int *)calloc(N, sizeof(int));
-    vector <int> neighbor[1000];
     for (i=0; i<N; i++) {
         sets[i] = i;
     }
@@ -185,10 +224,10 @@ pair<long int, int *> greedy(int N, double** points, long int** distances) {
         i = v[k].second.first;
         j = v[k].second.second;
         if (degrees[i]<=1 && degrees[j]<=1 && (sets[i]!=sets[j] || nedges==N-1)) {
-            neighbor[i].push_back(j);
-            neighbor[j].push_back(i);
             degrees[i]++;
             degrees[j]++;
+            edges[nedges].first = i;
+            edges[nedges].second = j;
             s = sets[i];
             if (nedges<N-1){
                 for (r=0; r<N; r++){
@@ -200,24 +239,8 @@ pair<long int, int *> greedy(int N, double** points, long int** distances) {
             length_tour += distances[i][j];
         }
     }
-    // Deduce the tour
-    int current = 0;
-    int prev = 0;
-    int next;
-    tour[0] = 0;
-    if (print)
-        printf("0\n");
-    for (k = 1; k<N; k++){
-        if (neighbor[current][0] == prev)
-            next = neighbor[current][1];
-        else
-            next = neighbor[current][0];
-        if (print)
-            printf("%d\n", next);
-        prev = current;
-        current = next;
-        tour[k] = next;
-    }
+
+    // Print
     if (print_length)
         printf("Length tour = %ld\n", length_tour);
     if (print_time) {
@@ -226,7 +249,9 @@ pair<long int, int *> greedy(int N, double** points, long int** distances) {
         if (print_time)
             printf("Greedy took %ld microseconds\n", elapsed);
     }
-    return enhance(N, distances, length_tour, tour);
+
+    // Enhance and return
+    return enhance(N, distances, length_tour, edges);
 }
 
 // Clarke Wright tour
@@ -235,8 +260,14 @@ pair<long int, int *> clarke_wright(int N, double** points, long int** distances
         printf("clarke-write\n");
     clock_t start_greedy = clock();
 
-    int* tour = (int *)malloc(N*sizeof(int));
+    // If N <= 2
+    if(N <= 2){
+        int *tour = (int *)malloc(N*sizeof(int));
+        return make_pair(1000000000, tour);
+    }
+
     int *degrees = (int *)calloc(N, sizeof(int));
+    pair<int, int> edges[1000];
     long int length_tour = 0;
 
     int hub = rand() % N;
@@ -268,7 +299,6 @@ pair<long int, int *> clarke_wright(int N, double** points, long int** distances
 
     // Union find
     int *sets = (int *)calloc(N, sizeof(int));
-    vector <int> neighbor[1000];
     for (i=0; i<N; i++) {
         sets[i] = i;
     }
@@ -279,8 +309,8 @@ pair<long int, int *> clarke_wright(int N, double** points, long int** distances
         if (degrees[i]<=1 && degrees[j]<=1 && sets[i]!=sets[j]) {
             if (print)
                 printf("Edge taken : (%d, %d), shortcut = %d\n", i, j, v[k].first);
-            neighbor[i].push_back(j);
-            neighbor[j].push_back(i);
+            edges[nedges].first = i;
+            edges[nedges].second = j;
             degrees[i]++;
             degrees[j]++;
             s = sets[i];
@@ -294,47 +324,25 @@ pair<long int, int *> clarke_wright(int N, double** points, long int** distances
             length_tour += distances[i][j];
         }
     }
-
-    if(N <= 2) return make_pair(1000000000, tour);
-
-    // Deduce the tour
-    tour[0] = hub;
+    // Add the last edges
     if (print)
         printf("%d\n", hub);
-    int first = -1;
-    for(i=0; i<N && first == -1; i++) {
+    int node1 = -1;
+    int node2 = -1;
+    for(i=0; i<N && node1 == -1; i++) {
         if (degrees[i]==1) {
-            first = i;
+            if (node1 == -1)
+                node1 = i;
+            else
+                node2 = i;
         }
     }
-    tour[1] = first;
-    if (print)
-        printf("%d\n", first);
-    length_tour += distances[first][hub];
-    int current = first;
-    int prev = hub;
-    int next;
-    for (k = 2; k<N; k++){
-        if (neighbor[current][0] == prev)
-            next = neighbor[current][1];
-        else
-            next = neighbor[current][0];
-        if (print)
-            printf("%d\n", next);
-        prev = current;
-        current = next;
-        tour[k] = next;
-    }
-    length_tour += distances[current][hub];
-    if (print_length)
-        printf("Length tour = %ld\n", length_tour);
-    if (print_time) {
-        clock_t end_greedy = clock();
-        long elapsed = timediff(start_greedy, end_greedy);
-        if (print_time)
-            printf("Clarke-Wright took %ld microseconds\n", elapsed);
-    }
-    return enhance(N, distances, length_tour, tour);
+    edges[nedges].first = hub;
+    edges[nedges].second = node1;
+    edges[nedges+1].first = node2;
+    edges[nedges+1].second = hub;
+
+    return enhance(N, distances, length_tour, edges);
 }
 
 // Optimal tour : brute force
@@ -358,12 +366,13 @@ int* brute_force(int N, double** points, long int** distances){
 }
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
     /* Get arguments */
     int N, i;
     scanf("%d", &N);
     //N = 1000;
     double **points = init_matrix_double(N, 2);
-
     /*
     int x = time(NULL) % 1000;
     x = 38;
@@ -374,11 +383,7 @@ int main(int argc, char *argv[]) {
         points[i][0] = rand() % 1000;
         points[i][1] = rand() % 1000;
     }
-
     */
-
-    srand(time(NULL));
-
     for (i=0; i<N; i++) {
         scanf("%lf", points[i]);
         scanf("%lf", points[i]+1);
@@ -392,7 +397,7 @@ int main(int argc, char *argv[]) {
 
     /* Find tours */
     pair <long int, int*> tour_nn = nearest_neighbor(N, points, distances);
-    pair <long int, int*> tour_greedy = greedy(N, points, distances);
+    /*pair <long int, int*> tour_greedy = greedy(N, points, distances);
 
     long int test_length = 1000000000;
     pair <long int, int*> tour_cw;
@@ -402,20 +407,23 @@ int main(int argc, char *argv[]) {
             tour_cw = tour_cw_test;
             test_length = tour_cw_test.first;
         }
-    }
+    }*/
 
     //int* tour_brute = brute_force(N, points, distances);
 
     /* Find the best one */
-    //long int best_length = min(tour_nn.first, tour_greedy.first);
-    long int best_length = min(tour_nn.first, min(tour_greedy.first, tour_cw.first));
+    //long int best_length = min(tour_nn.first, min(tour_greedy.first, tour_cw.first));
+    long int best_length = tour_nn.first;
     int *best_tour;
+    /*
     if (best_length == tour_nn.first)
         best_tour = tour_nn.second;
     else if (best_length == tour_greedy.first)
         best_tour = tour_greedy.second;
     else
         best_tour = tour_cw.second;
+    */
+    best_tour = tour_nn.second;
 
     /* Print the best tour */
     for (i=0; i<N; i++){
